0197cabf912a6c0436b56a4fd710f3284183d3a2
leandromds <leandro.mds.21@gmail.com>
2021-03-17 11:24:46 -0300

    implemented jwt token on register user service

--- src/routes/user/services/index.js
+++ src/routes/user/services/index.js
@@ -1,6 +1,15 @@
+const logger = require('../../../config/logger')
+const UserModel = require('../models/index')
+const { sign } = require('../../../utils/jwt')
+
 const UserServices = (() => {
   const register = async (userData) => {
-    return userData
+    const dbRes = await UserModel.create(userData)
+    const { password, ...user } = dbRes.toObject()
+    const token = sign({ user: user.id })
+
+    logger.info({ user, token })
+    return { user, token }
   }
 
   return {
