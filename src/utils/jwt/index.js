ee5c10d553f9bacb9b9a6fc5d2ff35b710c6ddd3
leandromds <leandro.mds.21@gmail.com>
2021-08-04 21:57:38 -0300

    update: inproviments on auth function

--- src/utils/jwt/index.js
+++ src/utils/jwt/index.js
@@ -11,19 +11,21 @@ const sign = payload => jwt.sign(payload, secret, option)
 const verify = token => jwt.verify(token, secret)
 
 const auth = async (req, res, next) => {
-  const [,token] = req.headers.authorization.split(' ')
+  if(!req.headers?.authorization) return res.sendStatus(401)
+
   try {
+    const [,token] = req.headers.authorization.split(' ')
     const payload = await verify(token)
     const user = await UserModel.findById(payload.user)
 
-    if (!user) return res.send(401)
+    if (!user) return res.sendStatus(401)
 
     req.auth = user
     
     next()
   } catch (error) {
     logger.error(error)
-    return res.status(4021).send({ status: false, message: 'Deu merda aqui' })
+    return res.sendStatus(401)
   }
 }
 
