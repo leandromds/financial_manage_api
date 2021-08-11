0ad8552c87f45b5617543ddb5ebc41a2e8c338db
leandromds <leandro.mds.21@gmail.com>
2021-08-10 21:39:37 -0300

    remove: all test

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,6 +3,7 @@ const pkg = require('../../../package.json')
 const routes = router => {
   router.get('/', (req, res) => {
     res.status(200).send({
+      status: true,
       message: 'Welcome to baseapi',
       version: pkg.version
     })
