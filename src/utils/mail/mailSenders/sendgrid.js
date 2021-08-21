320b54295d0625139171ad9889a54b9efab42eb8
leandromds <leandro.mds.21@gmail.com>
2021-08-20 22:39:06 -0300

    update: Add utils to send emails

---
+++ src/utils/mail/mailSenders/sendgrid.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
+
+const sendgrid = (() => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'test@example.com', // Change to your recipient
+    from: 'test@example.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>Email sent via sendGrid mailer</strong>',
+  }
+
+  const send = async () => {
+    console.log('using sendgrid...')
+    try {
+      await sgMail.send(msg)
+      return 'email sent'
+    } catch (error) {
+      console.error('error:', error)
+    }
+  }
+
+  return {
+    send
+  }
+
+})()
+
+module.exports = sendgrid
